a703e86f0cfec726ee4504989fed34c1b18a40cf
joe20182 <joe790924@gmail.com>
2022-07-03 13:09:19 +0800

    add: react-use-websocket

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import useWebSocket from "react-use-websocket";
+import "./App.css";
+import OrderBook from "./components/OrderBook";
 
 function App() {
+  const {
+    sendMessage,
+    sendJsonMessage,
+    lastMessage,
+    lastJsonMessage,
+    readyState,
+    getWebSocket,
+  } = useWebSocket("wss://ws.btse.com/ws/oss/futures", {
+    onOpen: () => console.log("opened"),
+    onClose: () => console.log("closed"),
+    //Will attempt to reconnect on all close events, such as server shutting down
+    shouldReconnect: (closeEvent) => true,
+    onMessage: (e) => console.log(JSON.parse(e.data)),
+  });
+
+  useEffect(() => {
+    // console.log(getWebSocket);
+    // sendJsonMessage({
+    //   op: "subscribe",
+    //   args: ["update:BTCPFC"],
+    // });
+  }, [getWebSocket, sendJsonMessage]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <OrderBook />
     </div>
   );
 }
---
+++ src/components/OrderBook/index.js
@@ -0,0 +1,9 @@
+const OrderBook = () => {
+  return (
+    <div className="order-book">
+      <p>123</p>
+    </div>
+  );
+};
+
+export default OrderBook;
