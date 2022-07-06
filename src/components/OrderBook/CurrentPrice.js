5a2b2ed46b9ba03d9b59ca4c608c32e2dc73b0a8
joe20182 <joe790924@gmail.com>
2022-07-06 16:15:48 +0800

    add: current price styles

--- src/components/OrderBook/CurrentPrice.js
+++ src/components/OrderBook/CurrentPrice.js
@@ -1,13 +1,22 @@
 import { memo } from "react";
 import { addComma } from "../../utils/format";
 import useGetCurrentPrice from "../../hooks/useGetCurrentPrice";
+import ArrowImg from "./ArrowImg";
 
 const CurrentPrice = () => {
   const { currentPrice, previousPrice } = useGetCurrentPrice();
 
+  const arrowType =
+    currentPrice === previousPrice
+      ? "same"
+      : currentPrice > previousPrice
+      ? "increase"
+      : "decrease";
+
   return (
-    <h3 className="current-price">
-      {addComma(currentPrice && currentPrice.toFixed(1))} - {previousPrice}
+    <h3 className={`current-price ${arrowType}`}>
+      <span>{addComma(currentPrice && currentPrice.toFixed(1))}</span>
+      <ArrowImg />
     </h3>
   );
 };
